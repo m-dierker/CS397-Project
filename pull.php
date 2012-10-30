56bc538b8278aceb138958879beea2bca0f5a485
Matthew Dierker <mvd7793@gmail.com>
2012-10-29 17:59:33 -0700

    Fixing dumb pull bug

--- pull.php
+++ pull.php
@@ -19,7 +19,7 @@ $output = null;
 
 
 // Executes git pull to pull from master
-// exec("/usr/bin/git pull", $output);
+exec("/usr/bin/git pull", $output);
 
 foreach($output as $x) {
     echo $x . "\n<br>\n";

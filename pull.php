ef6ceda553ab565fb2df1c91dfc28dbf4f56ca0e
Matthew Dierker <mvd7793@gmail.com>
2012-10-29 17:52:24 -0700

    Added pull domain whitelist so we can't use on dev domains

--- pull.php
+++ pull.php
@@ -4,10 +4,22 @@
  * This script will pull the latest version of the site from GitHub's master branch. To run it, just visit /pull.php - Authentication will be added once I write a PHP class to verify FB logins, at which point we'll control it all through that. If there's an error when running the script, email Matthew with the error.
  */
 
+/*
+ * Each domain has to be specially configured (PHP 5.3x + CGI, not FastCGI) with Enhanced Security off to run the script. Don't add a domain to this list unless you know what you're doing.
+ */
+$allowedDomains = array('my.eatcumtd.com');
+
+// Make sure the domain is allowed
+if (!in_array($_SERVER['HTTP_HOST'], $allowedDomains)) {
+    die('You cannot use the pull script on this URL. Please try again.');
+}
+
 $output = null;
 
+
+
 // Executes git pull to pull from master
-exec("/usr/bin/git pull", $output);
+// exec("/usr/bin/git pull", $output);
 
 foreach($output as $x) {
     echo $x . "\n";

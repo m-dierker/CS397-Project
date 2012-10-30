05600de09b39671ab70d719e717bbaf46acaf755
Matthew Dierker <mvd7793@gmail.com>
2012-10-29 17:48:24 -0700

    Adding pretty comments

--- pull.php
+++ pull.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * This script will pull the latest version of the site from GitHub's master branch. To run it, just visit /pull.php - Authentication will be added once I write a PHP class to verify FB logins, at which point we'll control it all through that. If there's an error when running the script, email Matthew with the error.
+ */
+
 $output = null;
 
 // Executes git pull to pull from master

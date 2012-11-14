38b6a4e2c53a348b49146f3ad777a6eef42a149e
Matthew Dierker <mvd7793@gmail.com>
2012-11-13 22:18:02 -0800

    Adding draggable and resizable widgets

--- js/Widget.js
+++ js/Widget.js
@@ -15,7 +15,9 @@ function Widget(site) {
     document.body.appendChild(widget);
 
     // Configure it
-    $('#' + this.id).draggable().addClass('widget').width(100).height(100);
+    $('#' + this.id).addClass('widget').resizable().width(100).height(100).css('position','absolute').css('top', '50px').css('left', '20px');
+
+    $('.widget').draggable({stack: '.widget'});
 
     console.log("ID: " + this.id);
 }

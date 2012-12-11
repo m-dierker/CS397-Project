08e7acdc620a236ca6f11414dca663ff262c8f62
Matthew Dierker <mvd7793@gmail.com>
2012-12-11 09:15:42 -0800

    Removing stupidity

--- php/db/updatewidget.php
+++ php/db/updatewidget.php
@@ -8,15 +8,15 @@ try
     $connection = new Mongo('mongodb://cs397:cinda@ds041387.mongolab.com:41387/testmongo');
     $database   = $connection->selectDB('testmongo');
     $collection = $database->selectCollection('test');
-    $mID = new MongoId($id); 
+    $mID = new MongoId($id);
     $oldItem = $collection->findOne(array('_id' => $mID));
     foreach($_GET as $name => $value) {
 		if ($name !== "id")
 		{
-			$oldItem[$name] = $value;		
-		} 
+			$oldItem[$name] = $value;
+		}
 	}
-    $oldItem['WidgetType'] = "BusWidget";
+    // $oldItem['WidgetType'] = "BusWidget";
     $collection->save($oldItem);
     echo json_encode($oldItem);
 }

c102adbabbf5172cf3e956b84527303531ffefba
Matthew Dierker <mvd7793@gmail.com>
2012-12-16 10:19:30 -0800

    Working on bus widget

--- js/widgets/cumtd/bus.js
+++ js/widgets/cumtd/bus.js
@@ -5,20 +5,32 @@ window.onload = function() {
         $.getJSON('http://developer.cumtd.com/api/v2.1/json/GetDeparturesByStop?key=c519e892e46841b8957ef39461faa6fb&stop_id=' + id + '&callback=?', function(data) {
 
             data['departures'].forEach(function(departure) {
-                addResult(departure['headsign'], departure['expected_mins'], departure['route']);
+                addResult(departure);
             });
         });
     }
 
-    function addResult(headsign, expected_mins, route) {
-        $('.bus-results').append('<div class="bus-result"><span class="bus-route-name">' + headsign + '</span><span class="bus-route-expectedmin">' + expected_mins + ' minutes</span><hr></div>');
+    function addResult(departure) {
+        $('.bus-results').append('<div class="bus-result"><span class="bus-route-name">' + departure['headsign'] + '</span><span class="bus-route-expectedmin">' + departure['expected_mins'] + ' minutes</span><div class="bus-pulldown"><div class="bus-route-longname"><strong>Route: </strong><span class="bus-route-colored" style="color: #' + departure['route']['route_color'] +'">' + departure['route_long_name'] + '</span></div><span class="bus-scheduled"><strong>Scheduled:</strong> ' + getFormattedTime(departure['scheduled']) + '</span><span class="bus-expected"><strong>Expected:</strong> ' + getFormattedTime(departure['expected']) + '</span></div></div><hr>');
     }
 
     function clearResults() {
-        $('.bus-results').empty();
+        $('.bus-results').empty().append('<hr class="top-line">');
     }
 
-    $.getJSON('http://developer.cumtd.com/api/v2.1/json/GetStops?key=c519e892e46841b8957ef39461faa6fb&callback=?', function(data) {
+    function getFormattedTime(time) {
+        // var date = Date.parse(time);
+        // time = '2012-12-16T12:12:00-06:00';
+        time = 'today';
+        var date = Date.parse(time);
+        console.log(date);
+
+        return date.toString('h:mm tt');
+
+    }
+
+    function loadStops() {
+        $.getJSON('http://developer.cumtd.com/api/v2.1/json/GetStops?key=c519e892e46841b8957ef39461faa6fb&callback=?', function(data) {
 
         var stops = data['stops'];
 
@@ -27,8 +39,22 @@ window.onload = function() {
         });
 
     });
+    }
+
+    loadStops();
+    loadStopsForID("PLAZA");
 
     $('body select').change(function() {
         loadStopsForID($('body select').val());
+    });
+
+    $('.bus-results .bus-result').click(function() {
+        console.log("click");
+        var pulldown = $(this).children('.bus-pulldown');
+        if($(pulldown).is(':hidden')) {
+            $(pulldown).slideDown();
+        } else {
+            $(pulldown).slideUp();
+        }
     });
 }

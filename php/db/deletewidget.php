db85aeedefced0a88bbe02e9981111fef0045a5f
bedoherty <bedoher2@illinois.edu>
2012-12-16 01:16:21 -0600

    Adding deletion widget.

---
+++ php/db/deletewidget.php
@@ -0,0 +1,18 @@
+<?php
+
+if (array_key_exists("id", $_GET) == false) die();
+
+$id = $_GET['id'];
+try
+{
+    $connection = new Mongo('mongodb://cs397:cinda@ds041387.mongolab.com:41387/testmongo');
+    $database   = $connection->selectDB('testmongo');
+    $collection = $database->selectCollection('test');
+    $collection->remove(array('_id' => new MongoId($id)), true);
+    echo "Removed item: " . $id;
+}
+catch(MongoConnectionException $e)
+{
+    die("Failed to connect to database ".$e->getMessage());
+}
+?>
